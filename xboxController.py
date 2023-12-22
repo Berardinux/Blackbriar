bed8f0838c22fd0b6a806ca3c1cf00b3fb96a0dd
Berardinux <Berardinux@gmail.com>
2023-12-22 20:54:37 +0000

    Dec 22, 15:54

---
+++ xboxController.py
@@ -0,0 +1,29 @@
+import evdev
+
+device = evdev.InputDevice('/dev/input/event0')
+
+print(device)
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_KEY:
+        if event.value == 1:
+            if event.code == evdev.ecodes.BTN_A:
+                print("Button A pressed")
+            elif event.code == evdev.ecodes.BTN_B:
+                print("Button B pressed")
+            elif event.code == evdev.ecodes.BTN_X:
+                print("Button X pressed")
+            elif event.code == evdev.ecodes.BTN_Y:
+                print("Button Y pressed")
+    elif event.type == evdev.ecodes.EV_ABS:
+        # Left Analog Stick
+        if event.code == evdev.ecodes.ABS_X:
+            print(f"Joystick X: {event.value}")
+        elif event.code == evdev.ecodes.ABS_Y:
+            print(f"Joystick Y: {event.value}")
+        # Right Trigger
+        elif event.code == evdev.ecodes.ABS_GAS:
+            print(f"Right Trigger: {event.value}")
+        # Left Trigger
+        elif event.code == evdev.ecodes.ABS_BRAKE:
+            print(f"Left Trigger: {event.value}")
