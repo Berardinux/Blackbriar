0f32ea053b7e1b7ee661da838683f130eb82b9b9
Berardinux <Berardinux@gmail.com>
2023-12-24 05:11:28 +0000

    Dec 24, 23:11

---
+++ servoXbox.py
@@ -0,0 +1,33 @@
+# Servo
+import RPi.GPIO as GPIO
+import time
+# Controller
+import evdev
+import numpy as np
+
+# Servo
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50)
+servo1.start(89)
+
+# Controller
+def map_value(value, in_min, in_max, out_min, out_max):
+    return np.interp(value, [in_min, in_max], [out_min, out_max])
+device = evdev.InputDevice('/dev/input/event0')
+print(device)
+
+for event in device.read_loop():
+    if event.type == evdev.ecodes.EV_ABS:
+        # Left Analog Stick
+        if event.code == evdev.ecodes.ABS_X:
+            var1 = event.value
+            angle = round(map_value(var1, 0, 65536, 0, 179))
+            print(angle)
+
+            #################################################
+            duty_cycle = 2 + (angle / 18)
+            servo1.ChangeDutyCycle(duty_cycle)
+            #time.sleep(0.1)
+            #var1 = round(event.value / 64)
+            #print(f"Joystick X: {event.value}")
