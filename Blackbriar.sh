93774e902bddd649a4d38ae0fd4df957e1f5bf60
Berardinux <Berardinux@gmail.com>
2023-12-26 07:02:40 +0000

    Dec 26, 2:02

--- Blackbriar.sh
+++ Blackbriar.sh
@@ -6,8 +6,10 @@ output=$(ls /dev/input | grep event0)
 # Check if the output is not empty
 if [ -n "$output" ]; then
   echo "Command produced output: $output"
-  python3 /home/berardinux/Documents/Blackbriar/dcMotorXbox.py &
-  python3 /home/berardinux/Documents/Blackbriar/servoXbox.py &
+  /usr/bin/python3 /home/berardinux/Documents/Blackbriar/src/dcMotorXbox.py & disown
+  /usr/bin/python3 /home/berardinux/Documents/Blackbriar/src/servoXbox.py & disown
+
+  
   while [ -n "$output" ]; do
     output=$(ls /dev/input | grep event0)
     echo "Still connected"
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ connect XX:XX:XX:XX:XX:XX
 
 # Look at controller raw output
 sudo evtest /dev/input/event0
+
+# udev rules
+/etc/udev/rules.d/99-Blackbriar.rules
+sudo udevadm control --reload-rules
+
+# Systemd
+/etc/systemd/system/blackbriar-monitor.service
+sudo systemctl daemon-reload
+sudo systemctl status blackbriar-monitor.service
