1f0ec848628a2c4b5395e297912149b2dc26e18d
Berardinux <Berardinux@gmail.com>
2023-12-26 01:35:37 +0000

    Dec 25, 20:35

--- test/bash/connect.sh
+++ test/bash/connect.sh
@@ -6,14 +6,8 @@ output=$(ls /dev/input | grep event0)
 # Check if the output is not empty
 if [ -n "$output" ]; then
   echo "Command produced output: $output"
-  python3 /home/berardinux/Documents/Blackbriar/dcMotorXbox.py &
-  python3 /home/berardinux/Documents/Blackbriar/servoXbox.py &
-  while [ -n "$output" ]; do
-    output=$(ls /dev/input | grep event0)
-    echo "Still connected"
-    sleep 1
-  done
-  
 else
   echo "Command did not produce any output."
+  python3 dcMotorXbox.py
+  python3 servoXbox.py
 fi
